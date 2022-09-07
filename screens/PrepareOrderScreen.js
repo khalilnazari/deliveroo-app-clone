a312421e23f7999a2b3d5004f2d75f153ea403ee
khalilnazari <khalil.deve@gmail.com>
2022-09-07 09:16:59 +0800

    packages are installed and screens are created

---
+++ screens/PrepareOrderScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PrepareOrderScreen = () => {
+  return (
+    <View>
+      <Text>PrepareOrderScreen</Text>
+    </View>
+  )
+}
+
+export default PrepareOrderScreen
