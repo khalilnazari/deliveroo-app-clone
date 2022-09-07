a1ce72c28c922be2bb378b71af61832a5b7808de
khalilnazari <khalil.deve@gmail.com>
2022-09-07 10:56:58 +0800

    Categories components is developed

--- components/Categories.js
+++ components/Categories.js
@@ -1,11 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import CategoryCard from './CategoryCard'
 
 const Categories = () => {
   return (
-    <View>
-      <Text>Categories</Text>
-    </View>
+    // horizonal scollar 
+    <ScrollView
+        contentContainerStyle={{
+            paddingHorizontal: 15, 
+            paddingTop: 10,
+        }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+    >
+      <CategoryCard imgUrl="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Cheeseburger-3d7c922.jpg" title="Burger" />
+      <CategoryCard imgUrl="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Cheeseburger-3d7c922.jpg" title="Burger" />
+      <CategoryCard imgUrl="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Cheeseburger-3d7c922.jpg" title="Burger" />
+      <CategoryCard imgUrl="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Cheeseburger-3d7c922.jpg" title="Burger" />
+      <CategoryCard imgUrl="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Cheeseburger-3d7c922.jpg" title="Burger" />
+      <CategoryCard imgUrl="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Cheeseburger-3d7c922.jpg" title="Burger" />
+      <CategoryCard imgUrl="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Cheeseburger-3d7c922.jpg" title="Burger" />
+      <CategoryCard imgUrl="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Cheeseburger-3d7c922.jpg" title="Burger" />
+      <CategoryCard imgUrl="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Cheeseburger-3d7c922.jpg" title="Burger" />
+      <CategoryCard imgUrl="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Cheeseburger-3d7c922.jpg" title="Burger" />
+      <CategoryCard imgUrl="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Cheeseburger-3d7c922.jpg" title="Burger" />
+    </ScrollView>
   )
 }
 
